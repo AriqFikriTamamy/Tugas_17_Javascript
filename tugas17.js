6aa87636d0477289a485a31aedb9aa1e9b8f4089
Ariq Fikri Tamamy <ariqrick@gmail.com>
2021-08-04 20:41:40 +0700

    Tugas 17 Javascript

---
+++ tugas17.js
@@ -0,0 +1,13 @@
+var kalimat = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+console.log(kalimat.toLowerCase())
+console.log(kalimat.toUpperCase())
+var kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual"
+console.log(kalimat2.toLowerCase())
+console.log(kalimat2.toUpperCase())
+
+function karakter(){
+  var dummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software";
+  console.log(dummy.charCodeAt(0)+ "-" +dummy.charCodeAt(1)+"-"+dummy.charCodeAt(2)+"-"+dummy.charCodeAt(3)+"-"+dummy.charCodeAt(4)+"-"+dummy.charCodeAt(5))
+}
+
+karakter()
